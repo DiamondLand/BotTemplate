705e3620e3f8afa314bf6a4fb691dfe8c634e1f3
DiamondLand <simoforstudio@gmail.com>
2025-01-17 11:32:58 +0300

    main template

---
+++ bot/config/cfg.py
@@ -0,0 +1,11 @@
+cfg = {
+    "SETTINGS": {
+        "token": "7923966358:AAHZ85Y8vCeYmREanwTGRYtVIEcmibmV9qg",
+        "testing_token": "6908785132:AAFD9nXAXSNO_LQSAqd5e_5zTqGDUMQHafw",
+        "version": "1.0.0",
+        "name": "В кипфин любят | бот"
+    }
+}
+
+CHANNEL_ID = 0
+ADMIN_CHATS = [872278858, 767922691, -1002149877782] # Я, Никита Владимирович, логи

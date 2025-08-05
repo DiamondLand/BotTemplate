08caf19cd93aeef71bc9924df938214eefef9d51
Diamond Land <81316675+DiamondLand@users.noreply.github.com>
2025-08-05 15:51:20 +0300

    refactor: abstract

---
+++ src/database/abstract.py
@@ -0,0 +1,29 @@
+import datetime
+
+from typing import Any
+from tortoise import Model, fields
+from tortoise.signals import pre_save
+
+
+async def pre_save_model(
+    sender: Any,
+    instance: "BaseORM",
+    using_db: str,
+    update_fields: list[str],
+) -> None:
+    instance.version += 1
+
+
+class BaseORM(Model):
+    id: int = fields.IntField(pk=True) 
+    created_at: datetime = fields.DatetimeField(auto_now_add=True)
+    updated_at: datetime = fields.DatetimeField(auto_now=True)
+    is_deleted: bool = fields.BooleanField(default=False) # Для возможности не удалять объекты из бд, но помечать их как удаленные
+    version: int = fields.IntField(default=0) # Для возможности отследить кол-во изменений объекта
+
+    def __init_subclass__(cls) -> None:
+        super().__init_subclass__()
+        pre_save(cls)(pre_save_model)
+
+    class Meta:
+        abstract = True
--- src/database/services.py
+++ src/database/services.py
@@ -3,12 +3,6 @@ from tortoise.transactions import atomic
 
 
 async def get_users_service():
-    """
-    Получает всех пользователей из базы данных.
-
-    Returns:
-        list: Список словарей с идентификаторами пользователей.
-    """
     users_data = await User.all()
     result = [{'user_id': entry.user_id} for entry in users_data]
     return result
@@ -16,15 +10,6 @@ async def get_users_service():
 
 @atomic()
 async def get_or_create_user_service(user_id: int):
-    """
-    Получает пользователя по идентификатору или создает нового, если он не существует.
-
-    Args:
-        user_id (int): Идентификатор пользователя.
-
-    Returns:
-        dict: Словарь с идентификатором пользователя.
-    """
     user_data = await User.get_or_none(user_id=user_id)
     if user_data is None:
         user_data = await User.create(user_id=user_id)
